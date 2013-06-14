e15d73844462ba6ac2e41769772255943af82257
Aaron <aschuenemann@gmail.com>
2013-06-13 19:20:22 -0500

    init

---
+++ char-count.js
@@ -0,0 +1,22 @@
+$(document).on('keyup keydown', '#YOUR-FORM-ID-HERE', function(e) {
+	
+  // Lengths
+  var maxLen = 140;
+  var warnLen = 10;
+  
+  // Colors
+  var defaultTextColor = {'color': ''};
+  var warnTextColor = {'color': 'red'};
+  var negTextColor = {'color': 'DarkRed'};
+  
+  var left = maxLen - $(this).val().length;
+  if(left > warnLen) {
+  	$('#char-count').html(left).css(defaultTextColor);
+  }
+  else if(left > 0){
+  	$('#char-count').html(left).css(warnTextColor);
+  }
+  else {
+  	$('#char-count').html(left).css(negTextColor);
+  }
+});
